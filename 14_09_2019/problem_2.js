10f2e1fbb565e250e4ad20e8bd4ae0130e397911
Binit Rai <binitcse@gmail.com>
2019-09-15 16:51:28 +0530

    Solved problem_2

--- 14_09_2019/problem_2.js
+++ 14_09_2019/problem_2.js
@@ -9,3 +9,11 @@ The o/p of the function should remain the same. the function should have followi
   } 
   Now call like sum.callAfter(1000, 5,10) => should invoke function sum after 1000 ms with param 5 and 10.
 */
+
+Function.prototype.callAfter = function(delay, ...args) {
+   var fn = this;
+   setTimeout(function() {
+	fn.apply(fn, args);	
+   }, delay)
+
+};
