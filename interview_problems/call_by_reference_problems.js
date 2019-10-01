7f7deeb004420e0e8292b8f51e24385e64a4779b
Binit Rai <binitcse@gmail.com>
2019-10-02 02:35:58 +0530

    Create call_by_reference_problems.js

---
+++ interview_problems/call_by_reference_problems.js
@@ -0,0 +1,12 @@
+function doubleArray(arr) {
+   for (let i in arr) {
+     arr[i] = arr[i] * 2; 
+  }
+  return arr;
+}
+
+const a1 = [2,3,4,5];
+const a2 = [3,5,6,7];
+
+let sum = a1[0] + a2[0];
+console.log(sum) ///?
