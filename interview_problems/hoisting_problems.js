f92e1b62b8887f07c9c3d55fe4b8758bcd438a81
Binit Rai <binitcse@gmail.com>
2019-10-02 10:35:04 +0530

    Update hoisting_problems.js

--- interview_problems/hoisting_problems.js
+++ interview_problems/hoisting_problems.js
@@ -1,6 +1,7 @@
 //hoisting problems
 /**********************************************************************/
 //Problem -3
+
 var bar = 1;
 function foo() {  
 	bar = 10;
@@ -9,6 +10,8 @@ function foo() {
 }
 foo();
 console.log(bar);  //?1 
+
+
 // You can ask the same question by moving function bar before return. 
 //This will not change the op Also you can define some code in bar body. Op will be still unchanged.
 
@@ -45,6 +48,7 @@ function foo() {
 } 
 foo(); //?
 
+
 /**********************************************************************/
 
 
@@ -61,6 +65,7 @@ var y;
 
 
 /**********************************************************************/
+
 //Problem 2:
 var z = 1;
 let z;
@@ -72,6 +77,7 @@ let z = 1;
 
 
 /**********************************************************************/
+
 //Problem 3:
 
 function a(){
@@ -85,6 +91,8 @@ a(); //?2
 
 
 /**********************************************************************/
+
+
 //Problem 4:
 alert(a());  //?3
 function a() {
@@ -107,5 +115,18 @@ function a() {
     return 8;
   }
 }
+
+/*
+
+Class declarations :
+Much like their function counterparts, JavaScript class declarations are hoisted. 
+However, they remain uninitialised until evaluation. 
+This effectively means that you have to declare a class before you can use it.
+
+https://scotch.io/tutorials/understanding-hoisting-in-javascript
+*/
+
+// https://www.freecodecamp.org/news/function-hoisting-hoisting-interview-questions-b6f91dbc2be8/
+// https://medium.com/@pvivek4/hoisting-demystified-with-popular-interview-questions-38a93ea441ff
 
 
