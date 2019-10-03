bf639da0f4f314b1cd7da84bceb70e1f4982dda7
Binit Rai <binitcse@gmail.com>
2019-10-03 11:17:41 +0530

    Create closure_problem.js

---
+++ interview_problems/closure_problem.js
@@ -0,0 +1,19 @@
+/*
+Write a function that would allow you to do this.
+
+foo(1)(2)(3) // 9
+foo(2)(2)(1) // 4
+foo(2,2,1) // 4
+foo() // 0
+
+*/
+
+
+/*
+Write a function that would allow you to do this.
+
+add(1)(2) // 3
+
+*/
+
+
