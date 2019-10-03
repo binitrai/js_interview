9e18fda4d73e1c575e0440e23001909b9c004d37
Binit Rai <binitcse@gmail.com>
2019-10-03 12:32:24 +0530

    Update closure_problem.js

--- interview_problems/closure_problem.js
+++ interview_problems/closure_problem.js
@@ -17,3 +17,15 @@ add(1)(2) // 3
 */
 
 
+//what will the following code output?
+const arr = [10, 12, 15, 21];
+for (var i = 0; i < arr.length; i++) {
+  setTimeout(function() {
+    console.log('Index: ' + i + ', element: ' + arr[i]);
+  }, 3000);
+}
+
+// Why? And how can you solve this problem?
+
+//How would you use a closure to create a private counter?
+
